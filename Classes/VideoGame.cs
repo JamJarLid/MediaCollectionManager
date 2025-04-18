71f6b1d43ca08d17685de39430a7e85df3009991
JamJarLid <espen.lever@gmail.com>
2025-04-18 22:50:43 +0200

    First class and enum created

---
+++ Classes/VideoGame.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaCollectionManager.Classes;
+
+public class VideoGame(string title, VideoGameConsole _console, DateOnly releaseYear, VideoGameVersion _videoGameVersion, bool owned, bool loaned)
+{
+  private string Title { get; set; } = title;
+  private VideoGameConsole console { get; set; } = _console;
+  private DateOnly ReleaseYear { get; set; } = releaseYear;
+  private VideoGameVersion videoGameVersion { get; set; } = _videoGameVersion;
+  private bool Owned { get; set; } = owned;
+  private bool Loaned { get; set; } = loaned;
+}
---
+++ Classes/VideoGameVersion.cs
@@ -0,0 +1,8 @@
+namespace MediaCollectionManager.Classes;
+
+public enum VideoGameVersion
+{
+  Standard,
+  Platinum,
+  Steelbook
+}
