70c42be0d748244002769f8c2e2f08cab9d77f6a
Espen Lever <espen.lever@gmail.com>
2026-02-03 22:19:11 +0100

    Initial CLI program menu added

--- Program.cs
+++ Program.cs
@@ -1,18 +1,48 @@
 ﻿namespace MediaCollectionManager;
 
-class VideoGame(string name)
-{
-  public string Name { get; private set; } = name;
-}
 class Program
 {
   static void Main(string[] args)
   {
-    VideoGame GodOfWar = new("God Of War");
+    bool running = true;
 
-    Console.WriteLine("Media Collection Manager");
-    Console.WriteLine("1. Add Video Game");
-    Console.WriteLine("2. View Video Games");
-    Console.WriteLine("3. Exit");
+    while(running)
+    {
+      Console.Clear();
+      Console.WriteLine("---JAMJARLIB MEDIA COLLECTION MANAGER---");
+      Console.WriteLine("1. Add video game");
+      Console.WriteLine("2. View video games");
+      Console.WriteLine("3. Exit");
+      Console.WriteLine();
+      Console.Write("Choose an option: ");
+
+      string? input = Console.ReadLine();
+
+      switch (input)
+      {
+        case "1":
+          Console.WriteLine("Selected Add Video Game (not implemented)");
+          Pause();
+          break;
+        case "2":
+          Console.WriteLine("Selected View Video Games (not implemented)");
+          Pause();
+          break;
+        case "3":
+          Console.WriteLine("Program exited");
+          running = false;
+          break;
+        default:
+          Console.WriteLine("Error: Invalid argument");
+          Pause();
+          break;
+      }
+    }
+  }
+  static void Pause()
+  {
+    Console.WriteLine();
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
   }
 }
