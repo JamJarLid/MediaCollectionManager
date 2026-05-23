cd47a42cd9a6b08e2ed24148ca1a761ab659a4a7
JamJarLid <espen.lever@gmail.com>
2026-05-23 17:43:14 +0200

    7.4: No primary constructor

--- Domain/MediaItem.cs
+++ Domain/MediaItem.cs
@@ -1,14 +1,19 @@
 namespace MediaCollectionManager.Domain;
 
-abstract class MediaItem(string title, int rating)
+abstract class MediaItem
 {
     private const int MinimumRating = 1;
     private const int MaximumRating = 10;
-    public string Title { get; private set; } = title;
+    public string Title { get; private set; }
 
-    public int Rating { get; private set; } = 
-        MinimumRating <= rating && rating <= MaximumRating ? 
-        rating : throw new ArgumentOutOfRangeException(
-            "Invalid rating number: Please choose a number between 1 and 10.");
+    public int Rating { get; private set; }
+
+    public MediaItem(string title, int rating)
+    {
+        Title = title;
+        Rating = MinimumRating <= rating && rating <= MaximumRating ? 
+            rating : throw new ArgumentOutOfRangeException(
+                "Invalid rating number: Please choose a number between 1 and 10.");
+    }
 
 }
